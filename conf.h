092e4b576dacf8d028d5106bbbd2cc6970347563
James Hughes <jhughes1010@gmail.com>
2022-11-09 13:55:53 -0700

    copy of prior project

---
+++ conf.h
@@ -0,0 +1,33 @@
+#define SerialMonitor
+//===========================================
+//LoRa band
+//===========================================
+#define BAND    915E6  //you can set band here directly,e.g. 868E6,915E6
+
+//===========================================
+//WiFi connection
+//===========================================
+char ssid[] = "ssid"; // WiFi Router ssid
+char pass[] = "password"; // WiFi Router password
+
+//===========================================
+//MQTT broker connection
+//===========================================
+//const char* mqttServer = "test.mosquitto.org";
+const char* mqttServer = "192.168.5.66";
+const int mqttPort = 1883;
+const char* mqttUser = "jhughes";
+const char* mqttPassword = "jhughes10";
+const char mainTopic[20] = "RoyalGorge/";
+
+//===========================================
+//Timezone information
+//===========================================
+const char* ntpServer = "pool.ntp.org";
+const long  gmtOffset_sec = -7 * 3600;
+const int   daylightOffset_sec = 3600;
+
+//===========================================
+//General defines
+//===========================================
+#define RSSI_INVALID -9999
